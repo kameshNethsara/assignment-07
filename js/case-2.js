4d453e457b3c81804a36a0bc2b2c7ad5dbd2d677
kameshNethsara <kamesh317nethsara@gmail.com>
2025-03-21 16:43:04 +0530

    Case-02 is done using HTML,CSS,JS(using jQuery)

---
+++ js/case-2.js
@@ -0,0 +1,58 @@
+$(document).ready(function () { 
+
+    // let boxes = document.querySelectorAll(".box");
+    let boxes = $(".box"); 
+
+    let currentIndex = 0;
+
+    function changeColor() {
+        // boxes.forEach((box) => {
+        //     box.style.backgroundColor = "white";
+        // });
+
+        // boxes[currentIndex].style.backgroundColor = "red";
+
+        // let previousIndex = (currentIndex - 1 + boxes.length) % boxes.length;
+        // boxes[previousIndex].style.backgroundColor = "green";
+        
+        // previousIndex = (currentIndex - 2 + boxes.length) % boxes.length;
+        // boxes[previousIndex].style.backgroundColor = "blue";
+        
+        // previousIndex = (currentIndex - 3 + boxes.length) % boxes.length;
+        // boxes[previousIndex].style.backgroundColor = "yellow";
+        
+        // previousIndex = (currentIndex - 4 + boxes.length) % boxes.length;
+        // boxes[previousIndex].style.backgroundColor = "pink";
+        
+        // previousIndex = (currentIndex - 5 + boxes.length) % boxes.length;
+        // boxes[previousIndex].style.backgroundColor = "violet";
+        
+        // currentIndex = (currentIndex + 1) % boxes.length;
+///////////////////////////////////////////////////////////////////////////////////////
+         boxes.css("background-color", "white");
+
+         boxes.eq(currentIndex).css("background-color", "red");
+ 
+         let previousIndex = (currentIndex - 1 + boxes.length) % boxes.length;
+         boxes.eq(previousIndex).css("background-color", "green");
+ 
+         previousIndex = (currentIndex - 2 + boxes.length) % boxes.length;
+         boxes.eq(previousIndex).css("background-color", "blue");
+ 
+         previousIndex = (currentIndex - 3 + boxes.length) % boxes.length;
+         boxes.eq(previousIndex).css("background-color", "yellow");
+ 
+         previousIndex = (currentIndex - 4 + boxes.length) % boxes.length;
+         boxes.eq(previousIndex).css("background-color", "pink");
+ 
+         previousIndex = (currentIndex - 5 + boxes.length) % boxes.length;
+         boxes.eq(previousIndex).css("background-color", "violet");
+ 
+         currentIndex = (currentIndex + 1) % boxes.length;
+
+
+    }
+
+    setInterval(changeColor, 1000);
+ 
+});
